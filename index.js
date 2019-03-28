4a040c3b8689214f58af24bcbd8fa4d5462b6a71
Kunal Ganglani <kunalganglani@gmail.com>
2019-03-28 15:29:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function fizzBuzz(start = 1, stop = 100)
+{
+    let result = '';
+    
+    if (stop < start || start < 0 || stop < 0) {
+        throw new Error('Invalid arguments');
+    }
+    
+    for (let i = start; i <= stop; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result += 'FizzBuzz';
+            continue;
+        }
+        
+        if (i % 3 === 0) {
+            result += 'Fizz';
+            continue;
+        }
+        
+        if (i % 5 === 0) {
+            result += 'Buzz';
+            continue;
+        }
+        
+        result += i;
+    }
+    
+    return result;
+}
+
+module.exports = {
+    fizzBuzz,
+}
